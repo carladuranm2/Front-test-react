b122a70014f03931d509fe5491c233600bdf792d
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2022-10-24 19:56:35 +0200

    feat: Create component Details

---
+++ src/pages/Detail-Product/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Details(){
+    return (
+        <div>
+            <h1>
+                Soy el details de cada producto
+            </h1>
+        </div>
+    )
+}
