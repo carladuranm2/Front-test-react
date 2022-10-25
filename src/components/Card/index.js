da089fa5d0796cc6d0efa034292eba6145c7a603
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2022-10-25 10:29:01 +0200

    feat: add card with products list

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,14 +6,14 @@ export default function Card(props){
     return (
         <div className="card-container">
         <div className="card">
-            {/* <div className="container" onClick={() => props.getValue(props.data.id)} > */}
-                {/* <h4><b>{props.data.name}</b></h4> */}
-                {/* <p>{props.data.description}</p> */}
-                <h5>{props.data.name}</h5>
-                {/* {props.isDetails && (
-                    <p><span>Details: </span>{props.data.details}</p>
-                )} */}
-            {/* </div> */}
+            <div className="container" onClick={() => props.getValueDetailId(props.data.id)} >
+                <h4><b>{props.data.brand}</b></h4>
+                <img src={props.data.imgUrl} alt="productPhone" />
+            </div>
+            <div>
+                <h5>{props.data.model}</h5>
+                <span><strong>${props.data.price}</strong></span>
+            </div>
             <div className="icon-actions">
                 {/* <button className="btn" onClick={() => props.deleteCard(props.data.id)}><i className="fa fa-trash"></i></button> */}
                 <button className="btn"><i className="fa fa-pencil"></i></button>
