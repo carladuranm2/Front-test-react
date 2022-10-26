36e467806abe8142db11d5da8ca349fb8909d0e2
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2022-10-26 19:45:29 +0200

    feat: call hook

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,12 @@
 import React from "react";
 import './index.css';
 import logo from '../../assets/image/logo.png';
+import addshopCar from '../../app/features/shopCar/shopCarSlice';
+import { useDispatch, useSelector } from 'react-redux';
 export default function Header () {
     const [topNav, settopNav] = React.useState(false);
+    const dispatch = useDispatch();
+    const cantProdut = useSelector(state => state.shopCar.cantProdut)
     function NavBarMobile () {
         settopNav(!topNav);
     }
@@ -16,7 +20,7 @@ export default function Header () {
                 </a>
                 <a href="/" className="cart">
                     <i className="fa fa-shopping-basket" aria-hidden="true"></i>
-                    <span id="cart_menu_num" data-action="cart-can" className="badge rounded-circle">5</span>
+                    <span id="cart_menu_num" data-action="cart-can" className="badge rounded-circle">{cantProdut}</span>
                 </a>
                 {/* <a href="/create" >Details</a> */}
                 {/* <a href="/create">More</a> */}
