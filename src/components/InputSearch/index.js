21be357ac954a88e4de6d36216c7ca8b6b140037
carladuranm2 <82276399+carladuranm2@users.noreply.github.com>
2022-10-26 19:45:43 +0200

    feat: add new function filter

--- src/components/InputSearch/index.js
+++ src/components/InputSearch/index.js
@@ -1,8 +1,19 @@
 
 import React from "react";
 import './index.css'
-export default function Search(){
+export default function Search({
+    filterProduct,
+    setProduct
+}){
+
+    const handleFilter = (e) => {
+        let expresion = new RegExp(`${e.target.value.toUpperCase()}.*`, "i");
+        let productFounds = filterProduct.filter(item => (expresion.test(item.brand) || expresion.test(item.model)));
+        setProduct(productFounds);
+        console.log(productFounds, " productofound");
+    }
+
     return (
-        <input className="TodoSearch" type="text" placeholder="Search.." />
+        <input className="TodoSearch" onChange={handleFilter} type="text" placeholder="Search.." />
     )
 }
